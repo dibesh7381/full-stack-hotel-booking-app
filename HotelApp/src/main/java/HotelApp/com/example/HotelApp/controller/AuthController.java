75e203c91f03fa491692d6fbc162b5db27bc72f4
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-10-22 12:07:44 +0530

    update frontend url

--- HotelApp/src/main/java/HotelApp/com/example/HotelApp/controller/AuthController.java
+++ HotelApp/src/main/java/HotelApp/com/example/HotelApp/controller/AuthController.java
@@ -544,7 +544,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "https://full-stack-hotel-booking-app-six.vercel.app", allowCredentials = "true")
 public class AuthController {
 
     private final AuthService authService;
